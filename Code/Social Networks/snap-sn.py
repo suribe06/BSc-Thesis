e6d437a0c75bdd5672e233beb6031d16eba5e034
suribe06 <santiu990506@gmail.com>
2021-08-18 21:27:13 -0500

    Pruebas con redes sociales, (netinf falla en windows)

--- Code/Social Networks/snap-sn.py
+++ Code/Social Networks/snap-sn.py
@@ -29,12 +29,39 @@ def intro():
             print("edge (%d %d)" % (NI.GetId(), Id))
 
 def prueba1():
-    G1 = snap.LoadEdgeList(snap.TNGraph,"prueba.csv",0,1, ',')
+    G1 = snap.LoadEdgeList(snap.TNGraph,"graph.txt",0,1, ',')
+
+    #Graph Information
     snap.PrintInfo(G1, "QA Stats", "qa-info.txt", False)
+
+    #SCC
+    print("Strongly Cnnected Components")
+    Components = G1.GetSccs()
+    for CnCom in Components:
+        print("Size of component: %d" % CnCom.Len())
+
+    #Degree Distribution
+    print("Degree Distribution")
+    DegToCntV = snap.TIntPrV()
+    snap.GetDegCnt(G1, DegToCntV)
+    for item in DegToCntV:
+        print("{0} nodes with degree {1}".format(item.GetVal2(), item.GetVal1()))
+
+    #Node Centrality (Page Rank)
+    print("PageRank of the Graph")
+    PRankH = snap.TIntFltH()
+    snap.GetPageRank(G1, PRankH)
+    for item in PRankH:
+        print(item, PRankH[item])
+
+    #Print Graph
     labels = {}
     for NI in G1.Nodes():
         labels[NI.GetId()] = str(NI.GetId())
     snap.DrawGViz(G1, snap.gvlDot, "G1.png", "G1", labels)
+
+def movielens_network():
+    N1 = snap.TNEANet.New() #directed network
 
 #intro()
 prueba1()
