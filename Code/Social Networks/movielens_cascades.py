a4c39686712db62c85322e3260c31e3317ce8fa9
suribe06 <santiu990506@gmail.com>
2021-10-03 23:42:08 -0500

    Cascades generator correction

--- Code/Social Networks/movielens_cascades.py
+++ Code/Social Networks/movielens_cascades.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pandas as pd
+import datetime
+import itertools
 
 movie_data = pd.read_csv('ratings_small.csv', usecols=[0, 1, 2, 3])
-
 M = movie_data['user_id'].nunique()
 N = movie_data['item_id'].nunique()
 movie_mapper = dict(zip(np.unique(movie_data["item_id"]), list(range(0,N)))) #Nodes 0 to N-1 are movies
 user_mapper = dict(zip(np.unique(movie_data["user_id"]), list(range(N,N+M))))#Nodes N to M-1 are users
-
-#print(movie_mapper)
-#print(user_mapper)
 
 for m in list(movie_mapper.values()):
     print("{0},{1}".format(m,m))
@@ -22,9 +20,16 @@ for idx, row in movie_data.iterrows():
     data = row.to_dict()
     m = movie_mapper[data['item_id']]
     u = user_mapper[data['user_id']]
-    t = data['timestamp']
-    cascades[m].append(u)
-    cascades[m].append(t)
+    t = datetime.datetime.fromtimestamp(data['timestamp'])
+    cascades[m].append([u, t])
+
+for key, value in cascades.items():
+    cascades[key].sort(key=lambda x: x[1], reverse=True)
+
+for key, value in cascades.items():
+    for i in range(len(value)):
+        value[i][1] = datetime.datetime.timestamp(value[i][1])
+    cascades[key] = list(itertools.chain(*value))
 
 print()
 for key, value in cascades.items():
