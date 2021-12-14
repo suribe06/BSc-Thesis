7eb7083a5798a0641da3c4b2458279a36ef9859f
suribe06 <santiu990506@gmail.com>
2021-12-14 02:37:41 -0500

    Recommendation results for 15 inferred network models

--- Code/Social Networks/netinf_tests.py
+++ Code/Social Networks/netinf_tests.py
@@ -1,12 +1,12 @@
 import os
 
 cascades = "movielens-cascades2.txt"
-n = 6
-name_output = "movielens-inferred-network-{0}".format(n)
-max_iter = 20000
-model = 2 #0:exponential, 1:powerlaw, 2:rayleigh
-expo = -12
-alpha = 1*10**(expo) #default:1
+num_model = 6
+name_output = "movielens-inferred-network-{0}".format(num_model)
+max_iter = 200000
+model = 1 #0:exponential, 1:powerlaw, 2:rayleigh
+expo = -5
+alpha = 7.5#1*10**(expo) #default:1
 
 #Execute netinf algorithm
 command = "./netinf -i:{0} -o:{1} -m:{2} -e:{3} -a:{4}".format(cascades, name_output, str(model), str(max_iter), str(alpha))
