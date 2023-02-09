8dec8b0db578cf3ed8d578d001f43e224fa0949f
suribe06 <santiu990506@gmail.com>
2023-02-08 21:04:41 -0500

    graphics changes

--- Code/Social Networks/article/graphics.py
+++ Code/Social Networks/article/graphics.py
@@ -11,7 +11,7 @@ alpha_arrange_expo, h = np.linspace(alpha_0, alpha_f, num=N, retstep=True)
 df_expo = pd.read_csv('inferred_edges_expo.csv', sep='|')
 axs[0].plot(alpha_arrange_expo, df_expo['inferred_edges_expo'].tolist(), 'm', label='Exponential Model')
 axs[0].set_xlabel("$\\alpha$")
-axs[0].set_ylabel('Inferred Edges')
+axs[0].set_ylabel('Number of inferred edges')
 axs[0].legend()
 
 N = 100
@@ -23,7 +23,7 @@ df_pow = pd.read_csv('inferred_edges_pow.csv', sep='|')
 axs[1].plot(alpha_arrange_pow, df_pow['inferred_edges_pow'].tolist(), 'g', label='Power-law Model')
 axs[1].set_xlabel("$\\alpha$")
 axs[1].axis(xmin=alpha_0-h,xmax=alpha_f+h)
-axs[1].set_ylabel('Inferred Edges')
+axs[1].set_ylabel('Number of inferred edges')
 axs[1].legend()
 
 N = 100
@@ -33,7 +33,7 @@ alpha_arrange_ray, h = np.linspace(alpha_0, alpha_f, num=N, retstep=True)
 df_ray = pd.read_csv('inferred_edges_ray.csv', sep='|')
 axs[2].plot(alpha_arrange_ray, df_ray['inferred_edges_ray'].tolist(), 'b', label='Rayleigh Model')
 axs[2].set_xlabel("$\\alpha$")
-axs[2].set_ylabel('Inferred Edges')
+axs[2].set_ylabel('Number of inferred edges')
 axs[2].set_xscale('log')
 axs[2].legend()
 
